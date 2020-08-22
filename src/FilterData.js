c146858f7636cefdc78b6f2c74e57c30950c915b
ellisandrews <ellisandrews1@gmail.com>
2020-08-22 15:42:20 -0500

    Add false postive rate to displayed filter data

--- src/FilterData.js
+++ src/FilterData.js
@@ -1,21 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 
-const FilterData = ({ itemSet }) => {
-  return (
-    <div id="filter-data">
-      <p>
-        Set Items: {'{ ' + itemSet.join(', ') + ' }'}
-      </p>
-    </div>
-  )
+class FilterData extends Component {
+  
+  getFalsePositiveRate = (k, m, n) => {
+    // Calculate the false positive rate of the filter
+    // k --> Number of hash functions
+    // m --> Number of bits in the array
+    // n --> Number of items added to the array
+    return (1 - (1 - 1/m)**(k*n))**k
+  }
+  
+  render() {
+
+    const { array, itemSet, numHashFunctions } = this.props
+    const falsePositiveRate = this.getFalsePositiveRate(numHashFunctions, array.length, itemSet.length)
+    const falsePostivePct = (falsePositiveRate * 100).toFixed(2)
+
+    return (
+      <div id="filter-data">
+        <p>
+          Set Items: {'{ ' + itemSet.join(', ') + ' }'}<br/>
+          False Positive Rate: {falsePostivePct}%
+        </p>
+      </div>
+    )
+  }
 }
 
 
 const mapStateToProps = state => {
   return {
-    itemSet: state.filter.itemSet
+    array: state.filter.array,
+    itemSet: state.filter.itemSet,
+    numHashFunctions: state.filter.numHashFunctions
   }
 }
 
