db1c3f7a35c04de36be965d8e6338ec4c479c296
ellisandrews <ellisandrews1@gmail.com>
2020-08-26 16:26:17 -0500

    Updates

---
+++ precache-manifest.770504ee36e04ddd712b4b4966b1264e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc42bfb4be5b947c0edda9a9b4eda22",
+    "url": "/bloom/index.html"
+  },
+  {
+    "revision": "ffaa3513e188a0763c7f",
+    "url": "/bloom/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "bb66dae553d9243c8f45",
+    "url": "/bloom/static/css/main.596f6191.chunk.css"
+  },
+  {
+    "revision": "ffaa3513e188a0763c7f",
+    "url": "/bloom/static/js/2.698d00a5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/bloom/static/js/2.698d00a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb66dae553d9243c8f45",
+    "url": "/bloom/static/js/main.3e6bdd8b.chunk.js"
+  },
+  {
+    "revision": "72f33716b5191d096f06",
+    "url": "/bloom/static/js/runtime-main.11f17837.js"
+  }
+]);
