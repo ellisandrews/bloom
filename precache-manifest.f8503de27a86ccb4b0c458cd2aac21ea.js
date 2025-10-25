a03b77872381f383faded3f87a1685dbf26f6c2d
ellisandrews-toast <ellis.andrews@toasttab.com>
2025-10-24 21:59:44 -0400

    Updates

---
+++ precache-manifest.f8503de27a86ccb4b0c458cd2aac21ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68a621be28ad7af645cd7917cb58a3e6",
+    "url": "/bloom/index.html"
+  },
+  {
+    "revision": "ffaa3513e188a0763c7f",
+    "url": "/bloom/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "4b58183cc8d11f8db34c",
+    "url": "/bloom/static/css/main.596f6191.chunk.css"
+  },
+  {
+    "revision": "ffaa3513e188a0763c7f",
+    "url": "/bloom/static/js/2.698d00a5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/bloom/static/js/2.698d00a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b58183cc8d11f8db34c",
+    "url": "/bloom/static/js/main.b35ce83e.chunk.js"
+  },
+  {
+    "revision": "72f33716b5191d096f06",
+    "url": "/bloom/static/js/runtime-main.11f17837.js"
+  }
+]);
